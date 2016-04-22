6d481c50940e9f6ec35e78922a2d843e34243596
DaveLoper <stygian@gmail.com>
2016-04-22 10:40:38 -0600

    Main script

---
+++ clearbox300g2-backup-updates.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+bp1=`/bin/cat /sys/bus/i2c/devices/6-0026/bypass0`
+bp2=`/bin/cat /sys/bus/i2c/devices/6-0022/bypass0`
+ovpnstatus=`/sbin/service openvpn status`
+ovpn=1
+
+if [[ $bp1 == "2" ]]; then
+	if [[ $bp2 == "2" ]]; then
+		:
+		if [[ $ovpnstatus == "Status written to /var/log/messages" ]]; then
+			ovpn=1
+			echo up
+		else
+			/sbin/service openvpn start
+			ovpn=2
+		fi
+		/usr/bin/yum -y update
+		/usr/sbin/clearcenter-checkin
+		
+		if [[ $ovpn == "2" ]]; then
+			/sbin/service openvpn stop
+		fi
+	fi
+fi
+
